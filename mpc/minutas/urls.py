d1d59cf9e8e9168f15a7fc923ac5472040a3f7b6
Matias-sh <devmatiasbritez@gmail.com>
2022-07-26 07:45:55 -0300

    A

--- mpc/minutas/urls.py
+++ mpc/minutas/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('minutas/', views.minutas, name="minutas"),
+    path('ing_mont_barrio/', views.ing_mont_barrio, name="ing_mont_barrio")
 ]
--- mpc/minutas/views.py
+++ mpc/minutas/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'minutas/index.html')
+
+def minutas(request):
+    return render(request, 'minutas/minutas.html')
+
+def ing_mont_barrio(request):
+    return render(request, 'minutas/ing_mont_barrio.html')
