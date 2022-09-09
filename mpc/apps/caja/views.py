bc751651305f6467c717e8280f252112b21d703e
Cocido <devmatiasbritez@gmail.com>
2022-09-09 19:32:41 -0300

    .

--- mpc/apps/caja/views.py
+++ mpc/apps/caja/views.py
@@ -13,18 +13,14 @@ def caja(request):
 def formulario_caja(request):
     if request.method == 'POST':
         form = CajaForm(request.POST)
-        form2 = DetMinutaForm(request.POST)
-        if form.is_valid() and form2.is_valid():
+        if form.is_valid():
             form.save()
-            form2.save()
             return redirect('vista_caja')
     else:
         form = CajaForm()
-        form2 = DetMinutaForm()
 
     context = {
         'form': form,
-        'form2': form2
     }
 
     return render(request, 'caja/cajona.html', context)
